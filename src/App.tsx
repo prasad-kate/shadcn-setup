047a6f34084ace1879c38a4d350cd690bc20bbf8
prasadk <prasadkatepk@gmail.com>
2024-09-11 23:27:18 +0530

    feat: add shadcn button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
-  return <div className="text-lg font-bold">Hello world</div>;
+  return (
+    <>
+      <div className="text-lg font-bold">Hello world</div>
+      <div className="flex items-center gap-2 my-2 justify-center">
+        <Button>Submit</Button>
+        <Button variant={"secondary"}>Submit</Button>
+        <Button variant={"outline"}>Submit</Button>
+        <Button variant={"link"}>Submit</Button>
+        <Button variant={"ghost"}>Submit</Button>
+        <Button variant={"destructive"}>Submit</Button>
+      </div>
+    </>
+  );
 }
 
 export default App;
